919cb3a1548263d1726acf5b20bd75d29932a402
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2024-12-25 15:42:01 -0300

    blog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,52 +6,31 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { AuthProviderBlog } from "@/contexts/AuthContextBlog";
 
-// Configuração da fonte local
+const API_URL = process.env.API_URL || "http://localhost:3333/";
+
 const geistSans = localFont({
   src: "./fonts/Poppins-Regular.ttf",
   variable: "--font-poppins-regular",
   weight: "100 900",
 });
 
-// Função para gerar metadados dinâmicos
 export async function generateMetadata(): Promise<Metadata> {
+  const response = await fetch(`${API_URL}configuration_blog/get_configs`, {
+    cache: "no-store", // Evita cache, se necessário
+  });
+  const blog = await response.json();
 
-  const API_URL = process.env.API_URL || "http://localhost:3333/";
-
-  try {
-    // Faz uma requisição para sua API
-    const response = await fetch(`${API_URL}` + "configuration_blog/get_configs");
-
-    if (!response.ok) {
-      throw new Error(`Erro na API: ${response.status} ${response.statusText}`);
-    }
-
-    const data = await response.json();
-
-    // Validação dos dados retornados
-    const nameBlog = data?.name_blog ?? "Blog"; // Usa "Blog" como fallback
-
-    // Retorna os metadados dinâmicos
-    return {
-      title: nameBlog,
-      description: nameBlog,
-    };
-  } catch (error) {
-    console.error("Erro ao buscar metadados:", error);
-
-    // Retorna valores padrão em caso de erro
-    return {
-      title: "Blog",
-      description: "Blog",
-    };
-  }
+  return {
+    title: blog?.name_blog || "Default Title",
+    description: blog?.description_blog || "Default Description",
+  };
 }
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="pt-br">
       <body id="root" className={`${geistSans.variable} antialiased`}>
